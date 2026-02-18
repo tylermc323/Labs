535b9143407d18bb6806ba14c6917dc907256c7e
tylermc323 <tylermc323@gmail.com>
2026-02-17 23:15:50 -0500

    Add primitives and testcases, add VS artifacts to gitignore

---
+++ PrimitiveTests/CompareArray_Test.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimitiveTests
+{
+    public class CompareArray_Test
+    {
+        //Test Criteria for CompareArray.cs, part of Codewars Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
+
+        //Solution
+    }
+}
---
+++ Primitives/CompareArray.cs
@@ -0,0 +1,12 @@
+﻿namespace Primitives
+{
+    public class CompareArray
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            // Codewars Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
+            
+            //Solution
+        }
+    }
+}
