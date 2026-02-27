6707095d9b18cfefb47a2c3edd45c9c1dc59e0c4
Z3nK41z3n <tylermc323@gmail.com>
2026-02-26 20:01:41 -0500

    ArrayDiff Kata complete

--- PrimitiveTests/CompareArray_Test.cs
+++ PrimitiveTests/CompareArray_Test.cs
@@ -1,5 +1,6 @@
 ﻿namespace PrimitiveTests
 {
+    
     public class CompareArray_Test
     {
         //Test Criteria for CompareArray.cs, part of Codewars Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
--- Primitives/CompareArray.cs
+++ Primitives/CompareArray.cs
@@ -1,12 +1,20 @@
-﻿namespace Primitives
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Primitives
 {
     public class CompareArray
     {
         public static int[] ArrayDiff(int[] a, int[] b)
         {
             // Codewars Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
-            
+
             //Solution
+
+            var bSet = new HashSet<int>(b);
+            return a.Where(x => !bSet.Contains(x)).ToArray();
+
         }
     }
 }
